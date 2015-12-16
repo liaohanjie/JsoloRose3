59dcebe4a2998efc9b4ed843640d55556c4aa2fa
jsolo <liaohanjie1314@126.com>
2015-12-16 18:45:55 +0800

    init

---
+++ src/com/heart/HeartHandler.java
@@ -0,0 +1,18 @@
+package com.heart;
+
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.handler.timeout.IdleStateAwareChannelHandler;
+import org.jboss.netty.handler.timeout.IdleStateEvent;
+/**
+ * 消息接受处理类
+ * @author -琴兽-
+ *
+ */
+public class HeartHandler extends IdleStateAwareChannelHandler {
+
+	@Override
+	public void channelIdle(ChannelHandlerContext ctx, IdleStateEvent e) throws Exception {
+		
+	}
+
+}
